f4a658e3e16aea017896ede01668a1229a22b59c
Nazmul8888 <nazmulkhan416@gmail.com>
2023-11-01 15:58:44 +0600

    token is done

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5001;
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials:true
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +26,6 @@ console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6nodxbc.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -32,29 +33,26 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
 // middlewares
-const logger = async(req,res,next)=>{
-  console.log('called:',req.host, req.originalUrl)
+const logger = (req,res,next)=>{
+  console.log('log info',req.method, req.url);
   next();
 }
-// verify token
 
-
-const verifyToken = async(req,res,next)=>{
-  const token = req.cookies?.token;
-  console.log('value of middleware',token)
+const verifyToken = (req, res, next)=>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware',token);
   if(!token){
-    return res.status(401).send({message: 'not authorized'})
+    return res.status(401).send({message:'unauthorized access'})
   }
-  next()
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decode)=>{
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err, decoded)=>{
     if(err){
-      console.log(err)
-      return res.status(401).send({message:'unauthorized'})
+      return res.status(401).send({message:'unauthorized access'})
     }
-  console.log('value in the token',decode)
-  req.user = decode;
-  next()
+    req.user = decoded;
+    next();
   })
   
 }
@@ -70,28 +68,35 @@ async function run() {
     const servicesCollection = client.db('doctor').collection('services');
     const checkOutCollection = client.db('doctor').collection('checkout')
 
-    // auth related api
-    app.post('/jwt', logger, async(req,res)=>{
+    // auth related api 1st step
+
+    app.post('/jwt',async(req,res)=>{
       const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
-        {expiresIn: '1h'})
+      console.log(user)
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'});
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none'
+      })
+      .send({success:true})
+    })
+    // auth related api second step
 
-        // how to token set
-
-        res
-        .cookie('token',token,{
-          httpOnly:true,
-          secure:false,
-          // sameSite:'none'
-
-        })
-      .send({success: true})
+    app.post('/logout',async(req,res)=>{
+      const user = req.body;
+      console.log('logging out',user);
+      res.clearCookie('token',{maxAge:0}).send({success:true})
     })
 
 
+
+
+
+
     // services related api
-    app.get('/services', logger, async(req,res)=>{
+    app.get('/services', async(req,res)=>{
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result)
@@ -113,9 +118,12 @@ async function run() {
   //  checkout 
 
 
-    app.get('/checkout', logger, verifyToken, async (req,res)=>{
+    app.get('/checkout',logger, verifyToken,async (req,res)=>{
     console.log(req.query.email);
-    // console.log('tok tok token', req.cookies.token)
+    console.log('token owner info', req.user)
+    if(req.user.email !== req.query.email){
+      return res.status(403).send({message:'forbidden access'})
+    }
     let query = {};
     if(req.query?.email){
       query = {email: req.query.email}
